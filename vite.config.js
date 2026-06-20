89a29bcdee472ee224ddafd3ef2a67fca5bacacc
Amartzi83 <amartzi83@users.noreply.github.com>
2026-06-20 16:10:42 +0300

    Revert to hashed JS filenames for reliable browser cache busting

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === 'production' ? '/trip-tracker/' : '/',
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/index.js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]',
-      }
-    }
-  }
 })
